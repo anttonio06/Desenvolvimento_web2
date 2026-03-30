e9f791a794ba6eee720c12f5ac1f011b76c0a576
anttoio06 <anttoniomacc@hotmail.com>
2026-03-29 21:38:52 -0300

    Novas atualizações

--- routes/servicos.routes.js
+++ routes/servicos.routes.js
@@ -14,4 +14,16 @@ router.get('/servicos', verificarAutenticacao, (req, res) => {
   });
 });
 
+router.put('/servicos/:id', verificarAutenticacao, (req, res) => {
+  const { duracao_min, preco_pequeno, preco_medio, preco_grande } = req.body;
+  db.run(
+    'UPDATE servicos SET duracao_min = ?, preco_pequeno = ?, preco_medio = ?, preco_grande = ? WHERE id = ?',
+    [duracao_min, preco_pequeno, preco_medio, preco_grande, req.params.id],
+    (err) => {
+      if (err) return res.status(500).json({ erro: err.message });
+      res.json({ ok: true });
+    }
+  );
+});
+
 module.exports = router;
